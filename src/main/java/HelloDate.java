ea8efc18f05c32bf3c56b9cfebcc20933bd16f54
yangwen-bo <1534458078@qq.com>
2019-10-29 17:40:21 +0800

    Think in JAVA 一 基本操作 日历对象操作

--- src/main/java/HelloDate.java
+++ src/main/java/HelloDate.java
@@ -18,11 +18,19 @@ public class HelloDate {
         DateFormat df1 = DateFormat.getDateInstance();//
         System.out.println(df1.format(begin));
 
+//        calendar.set(  );
         calendar.set( Calendar.HOUR_OF_DAY,23 );
         calendar.set( Calendar.MINUTE,59 );
         calendar.set( Calendar.SECOND,59 );
         Date end = calendar.getTime();
 
+        int i=0x1002;//十六进制
+        int y=03251;//八进制
+        System.out.println(Long.toBinaryString( i ));//二进制数形式显示
+        float f=1.39e-43f;//1.39*10^-43
+        System.out.println(f);
+//        printBinarInt(f);
+//        printBinarLong(f);
 
     }
 }
